74cc25de9f8b9fb5ced32d2e1609e9e3a0c62ef6
StefanoMRC <stefano.mrc92@gmail.com>
2022-04-28 15:36:37 +0200

    fine sezione 1

--- src/components/main/PrimaSezione/SezUno.vue
+++ src/components/main/PrimaSezione/SezUno.vue
@@ -1,117 +1,144 @@
 <template>
-  <div class="cont-principale" >
-    <div class="cont-secondario text-center" >
-      <p class="fs-1 w-50 m-auto">
-        Let passion and determination be the guide along the way and develop at your own pace that's comfortable.
-      </p>
-      <h4 class="text-uppercase pt-4">fannie moreno</h4>
-      <span class="color_chiaro">/Founder & CEO</span>
-      <div class="pallino_big p_green"></div>
-      <div class="pallino_small p_orange"></div>
-      <div class="pallino_big p_yellow"></div>
-      <div class="pallino_small p_purple"></div>
-      <div class="pallino_small p_blu"></div>
-      <div class="pallino_small p_red"></div>
-      <div class="pallino_small p_light_green"></div>
-      <div class="pallino_big p_light_orange"></div>
+  <div class="cont-principale">
+    <div class="cont-secondario text-center">
+      <div class="pb-4">
+        <p class="fs-1 w-50 m-auto">
+          Let passion and determination be the guide along the way and develop at your own pace that's comfortable.
+        </p>
+        <h4 class="text-uppercase pt-5">fannie moreno</h4>
+        <span class="color_chiaro ">/Founder & CEO</span>
+        <div class="pallino_big p_green"></div>
+        <div class="pallino_small p_orange"></div>
+        <div class="pallino_big p_yellow"></div>
+        <div class="pallino_small p_purple"></div>
+        <div class="pallino_small p_blu"></div>
+        <div class="pallino_small p_red"></div>
+        <div class="pallino_small p_light_green"></div>
+        <div class="pallino_big p_light_orange"></div>
+      </div>
+      <div class="container pt-5">
+        <div class="row">
+          <div v-for="(element, index) in listaDati" :key="index" class="col-3">
+            <h2 class="fs-1 color_green">{{element.numeri}}</h2>
+            <span class="text-uppercase color_chiaro">{{element.descrizione}}</span>
+          </div>
+        </div>
+
+      </div>
     </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'SezUno',
-  data() {
-    return {
-      listaDati:[
-        {
-          numeri:1.926,
-          descrizione:'finisched sessions'
-        },
-        {
-          numeri:3.092,
-          descrizione:'enrolled learners'
-        },
-        {
-          numeri:200,
-          descrizione:'online instructors'
-        },
-        {
-          numeri:'100%',
-          descrizione:'satisfaction rate'
-        },
-      ]
-    }
-  },
+  export default {
+    name: 'SezUno',
+    data() {
+      return {
+        listaDati: [{
+            numeri: 1.926,
+            descrizione: 'finisched sessions'
+          },
+          {
+            numeri: '3.092+',
+            descrizione: 'enrolled learners'
+          },
+          {
+            numeri: 200,
+            descrizione: 'online instructors'
+          },
+          {
+            numeri: '100%',
+            descrizione: 'satisfaction rate'
+          },
+        ]
+      }
+    },
 
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  .cont-principale{
+  .cont-principale {
     padding-top: 150px;
-    .cont-secondario{
+    background-image: url('../../../assets/img/background-pattern-grid-line.png');
+
+    .cont-secondario {
       width: 80%;
       margin: auto;
       padding-top: 150px;
       position: relative;
-      .color_chiaro{
+
+      .color_chiaro {
         color: rgba(0, 0, 0, 0.527);
       }
-      .pallino_big{
+
+      .pallino_big {
         height: 50px;
         width: 50px;
         border-radius: 50%;
         position: absolute;
       }
-      .pallino_small{
+
+      .pallino_small {
         height: 30px;
         width: 30px;
         border-radius: 50%;
         position: absolute;
       }
-      .p_green{
+
+      .p_green {
         border: 6px solid #1fad95b9;
         top: 0;
         left: 90px;
       }
-      .p_orange{
+
+      .p_orange {
         background: #e25a6cd2;
-        top: 50%;
-        left:150px;
+        top: 40%;
+        left: 150px;
       }
-      .p_yellow{
+
+      .p_yellow {
         background: #e2a957;
-        bottom: 0;
+        bottom: 100;
         left: 10px;
       }
-      .p_purple{
+
+      .p_purple {
         background: rgba(128, 0, 128, 0.582);
         left: 40%;
-        top:50px;
+        top: 50px;
       }
-      .p_light_green{
+
+      .p_light_green {
         background: rgba(32, 178, 171, 0.534);
         top: -65px;
         right: 210px;
         z-index: -1;
       }
-      .p_red{
+
+      .p_red {
         background: #e25a6cd2;
         top: 40%;
         right: 200px;
       }
-      .p_blu{
+
+      .p_blu {
         background: #1f9ffb7c;
-        top: 70%;
-        right: 350px;
+        top: 65%;
+        right: 300px;
       }
-      .p_light_orange{
+
+      .p_light_orange {
         border: 6px solid #e25a6c65;
-        bottom: 0;
-        right: 200px;
+        bottom: 55px;
+        right: 100px;
       }
+      .color_green{
+        color: #1fad96;
+        }
     }
   }
 </style>
